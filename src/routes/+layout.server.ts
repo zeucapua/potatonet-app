65ce338a8fcd9b017dc1d2e917726e45f10e24ab
zeudev <coding@zeu.dev>
2026-01-07 17:07:07 -0800

    refactor to quickslice oauth

--- src/routes/+layout.server.ts
+++
@@ -1,9 +0,0 @@
-import type { ServerLoadEvent } from "@sveltejs/kit";
-
-export async function load({ locals }: ServerLoadEvent) {
-  // have user available throughout the app via LayoutData
-  return !locals.user ? undefined : { user: {
-    did: locals.user.did,
-    handle: locals.user.handle
-  }};
-}
